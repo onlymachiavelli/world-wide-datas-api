b489b9ddff909e080ae578e5895f2d2019e89cea
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-18 23:38:25 +0100

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from "express"
-const app = express()
-const PORT = 3000
-app.get("/", (req, res) => {
+const cors = require("cors")
+const app: Application = express()
+
+app.use(cors())
+const PORT: Number = 3000
+app.get("/", (req: Request, res: Response): void => {
   res.send("HELLO WORLD")
 })
 
-app.listen(PORT, () => {
-  console.log("Working")
+app.listen(PORT, (): void => {
+  console.log(`Listening on ${PORT}`)
 })
