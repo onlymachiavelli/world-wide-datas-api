455f18bfc57bc9611c41baa13a04829b55e5db5f
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-18 23:51:42 +0100

    preparing dataJson

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, { Application, Request, Response } from "express"
 const cors = require("cors")
 const app: Application = express()
+const datas = require("./world.json")
 
 app.use(cors())
 const PORT: Number = 3000
 app.get("/", (req: Request, res: Response): void => {
-  res.send({
-    flag: "shit",
-  })
+  res.send(datas)
 })
 
 app.listen(PORT, (): void => {
